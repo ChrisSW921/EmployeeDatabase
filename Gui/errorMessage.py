3863fd899b8d43a582a8e30a270cbc45443c138e
Chris <withscottc921@gmail.com>
2020-10-29 21:32:11 -0600

    Fixed error message pop up

--- Gui/errorMessage.py
+++ Gui/errorMessage.py
@@ -8,15 +8,19 @@ class errorWindow:
         self.window = Tk()
         self.window.title("ERROR")
 
-        errorMessage = Label(text=self.message)
-        okButton = Button(text='OK')
+        self.frame1 = LabelFrame(self.window, text="", padx=20, pady=20)
 
-        errorMessage.grid(row=0, column=0)
-        okButton.grid(row=1,column=0, padx=15)
+        self.frame1.pack()
+
+        self.errorMessage = Label(self.frame1, text=self.message)
+        self.okButton = Button(self.frame1, text='OK', command=self.okButtonPressed)
+
+        self.errorMessage.grid(row=0, column=0)
+        self.okButton.grid(row=1,column=0, padx=15)
         
 
-        def okButtonPressed():
-            self.window.destroy()
-            print("Done")
+    def okButtonPressed(self):
+        self.window.destroy()
+        print("Done")
 
 
