5992cd08e4d6dc576e6868a029f4fe9c808fd906
Carson Stromberg <carson.stromberg@outlook.com>
2020-11-05 11:29:08 -0700

    Made some slight adjustments

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ import os
 sys.path.insert(0,os.getcwd())
 
 from setuptools import setup
-from Database import database
 
 setup(name='EmpDatabase',
       version='0.1',
       description='Employee Database',
       url='https://github.com/ChrisSW921/EmployeeDatabase',
       packages=['Backend', 'Database', 'Gui'],
-      install_requires=['pandas'],
+      install_requires=['pandas', 'openpyxl'],
       zip_safe=False)
-
-database.initialize_employee_database()
