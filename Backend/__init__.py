ca17c98f0e68b0e4772ddd55573cc7eb4d00cabd
Carson Stromberg <carson.stromberg@outlook.com>
2020-11-05 14:42:12 -0700

    Removed uncessary __init__ code

--- Backend/__init__.py
+++
@@ -1,6 +0,0 @@
-from .employee import Employee
-from .employee_address import EmployeeAddress
-from .employee_credentials import EmployeeCredentials
-from .employee_permissions import EmployeePermissions
-from .employee_pto import EmployeePTO
-from .employee_timecard import EmployeeTimecard
